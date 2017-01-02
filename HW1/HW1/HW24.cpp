9ac6a851cf0db9ba0d7550f5eb2b278f303f0859
Chung Dong Gun <chungdk1117@gmail.com>
2017-01-02 22:15:58 +0900

    HW24.cpp file is updated
    Signed-off-by: Chung Dong Gun <chungdk1117@gmail.com>

---
+++ HW1/HW1/HW24.cpp
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+int input();
+int escape(int);
+void output(int, int);
+
+int main()
+{
+	int deep = input();
+	int day = escape(deep);
+	output(deep, day);
+	return 0;
+}
+
+int input()
+{
+	int deep;
+	printf("* 우물의 깊이를 입력하시오(cm단위) : ");
+	scanf("%d", &deep);
+	return deep;
+}
+
+int escape(int x)
+{
+	int cnt = 0;
+	int i;
+
+	if (x == 0)
+		cnt = 0;
+	
+	else
+	{
+		for (;;)
+		{
+			cnt++;
+			x = x - 50;
+			if (x <= 0)
+				break;
+			else
+				x = x + 20;
+		}
+	}
+
+	return cnt;
+}
+
+void output(int deep,int day)
+{
+	printf("%.2lf 미터 깊이의 우물을 탈출하기 위해서는 %d일이 걸립니다.\n", (double)deep/100, day);
+
+}
